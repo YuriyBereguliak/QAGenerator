c8067550000fd0b87f67687a8e2093f341cf48dd
Yuriy Bereguliak <yuriy.bereguliak@inoxoft.com>
2018-12-01 22:16:59 +0200

    dev : created sentence factory.

--- generator.processor/src/com/bereguliak/processor/builder/question/SimpleQuestionGenerator.kt
+++ generator.processor/src/com/bereguliak/processor/builder/question/SimpleQuestionGenerator.kt
@@ -5,41 +5,28 @@ import com.bereguliak.generator.utility.GlobalConst.SimpleQuestions.Companion.SI
 import com.bereguliak.generator.utility.GlobalConst.SimpleQuestions.Companion.SIMPLE_WHAT_QUESTION
 import com.bereguliak.generator.utility.GlobalConst.Strings.Companion.DEFAULT_SIMPLE_QUESTION_TITLE
 import com.bereguliak.processor.builder.core.BaseBuilder
-import com.bereguliak.processor.model.entity.Question
+import com.bereguliak.processor.builder.question.factory.SentenceFactory
+import com.bereguliak.processor.builder.question.factory.SentenceTypeFactory
 import com.bereguliak.processor.model.entity.DataChain
-
+import com.bereguliak.processor.model.entity.Question
+import com.bereguliak.processor.model.enums.SentenceType
 
 class SimpleQuestionGenerator(data: DataChain) : BaseBuilder<Question>(data) {
     //region BaseBuilder
     override fun generate(): Question {
+        val sentenceFactory: SentenceFactory = SentenceTypeFactory()
         val result = data.ner
                 .distinct()
                 .withIndex()
                 .joinToString(separator = "\n") { (index, value) ->
-                    val words = numberOfWords(value.trim())
-                    when (words) {
-                        0 -> ""
-                        1 -> "${index + 1}) $SIMPLE_DEFINITION ${value.toLowerCase()}"
-                        2 -> "${index + 1}) $SIMPLE_WHAT_QUESTION ${value.toLowerCase()}"
-                        else -> "${index + 1}) $SIMPLE_DESCRIBE ${value.toLowerCase()}"
+                    when (sentenceFactory.define(value)) {
+                        SentenceType.UNDEFINED -> ""
+                        SentenceType.DEFINITION -> "${index + 1}) $SIMPLE_DEFINITION ${value.toLowerCase()}"
+                        SentenceType.WHAT -> "${index + 1}) $SIMPLE_WHAT_QUESTION ${value.toLowerCase()}"
+                        SentenceType.DESCRIBE -> "${index + 1}) $SIMPLE_DESCRIBE ${value.toLowerCase()}"
                     }
                 }
         return Question(DEFAULT_SIMPLE_QUESTION_TITLE, result)
-    }
-    //endregion
-
-    //region Utility API
-    private fun numberOfWords(trimmed: String): Int {
-        return if (trimmed.isEmpty()) {
-            0
-        } else {
-            trimmed.split("\\s+".toRegex())
-                    .dropLastWhile {
-                        it.isEmpty()
-                    }
-                    .toTypedArray()
-                    .size
-        }
     }
     //endregion
 }
---
+++ generator.processor/src/com/bereguliak/processor/model/enums/SentenceType.kt
@@ -0,0 +1,8 @@
+package com.bereguliak.processor.model.enums
+
+enum class SentenceType(val value:Int) {
+    UNDEFINED(0),
+    DEFINITION(1),
+    WHAT(2),
+    DESCRIBE(3)
+}
