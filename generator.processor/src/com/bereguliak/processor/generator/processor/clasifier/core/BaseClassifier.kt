aa41a0822454aa3b0db2b72946b352ddfc5b9aa3
Yuriy Bereguliak <yuriy.bereguliak@inoxoft.com>
2018-12-02 20:29:43 +0200

    dev : implemented base classifier.

--- generator.processor/src/com/bereguliak/processor/generator/processor/clasifier/core/BaseClassifier.kt
+++ generator.processor/src/com/bereguliak/processor/generator/processor/clasifier/core/BaseClassifier.kt
@@ -1,4 +1,24 @@
 package com.bereguliak.processor.generator.processor.clasifier.core
 
-class BaseClassifier {
+import com.bereguliak.maxent.model.MaxentDataModel
+import com.bereguliak.maxent.stream.PlainTextEventStream
+import com.bereguliak.processor.generator.core.chain.BaseGeneratorChain
+import opennlp.maxent.GIS
+import opennlp.maxent.GISModel
+
+abstract class BaseClassifier : BaseGeneratorChain() {
+
+    private lateinit var model: GISModel
+
+    //region BaseClassifier
+    protected fun buildModel(data: MutableList<MaxentDataModel>) {
+        val stream = PlainTextEventStream(data)
+        model = GIS.trainModel(stream, 100, 1, false, true)
+    }
+
+    protected fun eval(text: String): String {
+        val nerResult = model.eval(PlainTextEventStream.buildContext(text))
+        return model.getBestOutcome(nerResult)
+    }
+    //endregion
 }
